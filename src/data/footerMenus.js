2a15f2f7ff32be6d408b39ad912bde780e7d87e2
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-06 00:41:46 +0100

    add footer menu data

---
+++ src/data/footerMenus.js
@@ -0,0 +1,20 @@
+export const menu = [
+  {name: "Products", link: "#"},
+  {name: "Rooms", link: "#"},
+  {name: "Inspirations", link: "#"},
+  {name: "About Us", link: "#"},
+  {name: "Terms & Policy", link: "#"},
+];
+
+export const account = [
+  {name: "My Account", link: "#"},
+  {name: "Checkout", link: "#"},
+  {name: "My Cart", link: "#"},
+  {name: "My Catalog", link: "#"},
+];
+
+export const stayConnected = [
+  {name: "Facebook", link: "#"},
+  {name: "Instagram", link: "#"},
+  {name: "Twitter", link: "#"},
+];
