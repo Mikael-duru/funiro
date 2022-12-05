2134defbe3f17a9c475b03c2fd8cd4642e4010fc
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-06 00:50:03 +0100

    add code that handles the app’s navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Header from "./components/Header";
+import Footer from "./components/footer/Footer"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <main>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </main>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
