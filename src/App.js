a5fee7da0717c0693a7cf168e8228e8995703d8c
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-12 14:46:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Cart from "./pages/Cart";
-import Header from "./components/Header";
-import Footer from "./components/footer/Footer"
+// import Route when you uncomment element within the Routes, and uncomment element import from its folder
+import { BrowserRouter as Router, Routes} from "react-router-dom";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
+// import Cart from "./pages/Cart";
+// import Header from "./components/Header";
+// import Footer from "./components/footer/Footer"
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
+      {/* <Header /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/products" element={<Products />} /> */}
+        {/* <Route path="/cart" element={<Cart />} /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
