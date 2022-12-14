847ad7aa966ce9315bcab327cf7c3570af31be43
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-14 18:18:54 +0100

    add hero background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@ const Hero = () => {
   return (
     <section className="container mx-auto">
       <div className="bg-[#F9F1E7]">
-        
+        <div className="pt-2 md:pt-20 px-[5px] sm:px-2 lg:pl-[5px] lg:pr-[13px]">
+          <div className="bg-hero bg-no-repeat bg-left-bottom bg-contain md:bg-cover object-contain h-[165px] sm:h-[250px] md:h-[300px] lg:h-[553px] w-full">
+
+          </div>
+        </div>
       </div>
     </section>
   );
