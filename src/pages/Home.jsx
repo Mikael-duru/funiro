47def443fe887bc59055148178e8c12db03f70fa
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-14 10:21:18 +0100

    add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Features from "../components/Features";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <main>
       <Features />
+      <Hero />
     </main>
   );
 }
