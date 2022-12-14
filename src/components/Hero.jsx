53afd49a0fabe3a7048ed70c6e3134616ff24518
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-14 14:27:05 +0100

    add hero section UI

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="bg-[#F9F1E7]">
+        
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
