54548386f13f5482fe9b34a3679d4e0e4beef621
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-12-14 10:20:31 +0100

    Container and Card Height Adjusted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import Route when you uncomment element within the Routes, and uncomment element import from its folder
 import { BrowserRouter as Router, Routes} from "react-router-dom";
 // import Home from "./pages/Home";
-import Products from "./components/Products/Products";
 // import Cart from "./pages/Cart";
 // import Header from "./components/Header";
 // import Footer from "./components/footer/Footer"
@@ -10,7 +9,6 @@ import './App.css';
 function App() {
   return (
     <Router>
-    <Products />
       {/* <Header /> */}
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
