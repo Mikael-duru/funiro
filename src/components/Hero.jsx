1754b30acf731d283bf8f2c75b33bd31209605cc
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-16 22:04:23 +0100

    add hero bg padding top for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="container mx-auto">
       <div className="bg-[#F9F1E7]">
-        <div className="sm:pt-20 px-[5px] sm:px-2 lg:pl-[5px] lg:pr-[13px] relative">
+        <div className="pt-6 sm:pt-20 px-[5px] sm:px-2 lg:pl-[5px] lg:pr-[13px] relative">
           <div className="bg-hero bg-no-repeat bg-left-bottom bg-cover object-contain h-[145px] sm:h-[250px] md:h-[300px] lg:h-[375px] xl:h-[500px] xxl:h-[553px] w-full">
             <div className="xxl:w-[294px] sm:absolute sm:right-4 sm:bottom-4 md:right-7 md:bottom-6 lg:right-20 lg:bottom-10 xl:right-[122px] xxl:right-[143px]">
               <div className="pt-4 pl-3 lg:pl-4 xl:pl-6  sm:pb-14 md:pb-16 lg:pb-20 xl:pb-28 xxl:pb-[138.52px]">
@@ -17,12 +17,12 @@ const Hero = () => {
               </button>
             </div>
           </div>
-          <div className="py-10 px-3 sm:py-0 sm:absolute sm:top-0 md:pl-4 xxl:pl-[100px]">
+          <div className="pt-10 pb-6 px-3 sm:py-0 sm:absolute sm:top-0 md:pl-4 xxl:pl-[100px]">
             <div className="mx-auto w-full sm:w-[270px] md:w-[304px] lg:w-[404px] xl:w-[494px] bg-[rgba(255,255,255,.72)] backdrop-filter backdrop-blur">
               <div className="py-6 px-6 sm:py-4 md:py-6 lg:p-9 xl:pl-12 xxl:py-[60px] xxl:pl-[60px]">
                 <h1 className="font-bold text-2xl md:text-4xl lg:text-[40px] lg:leading-tight xl:text-[3.5rem] xl:leading-[4.1875rem] pb-3 lg:pb-4 text-darkGray">High-Quality Furniture Just For You</h1>
                 <p className="font-black text-base lg:text-lg xl:text-xl text-grayThree xl:pr-[4.8125rem] pb-4 lg:pb-[1.125rem]">Our furniture is made from selected and best quality materials that are suitable for your dream home</p>
-                <button className="py-3 px-10 lg:py-4 xl:py-6 xl:pl-[140px] xl:pr-[132px] bg-primary cursor-pointer hover:bg-transparent hover:border-2 hover:border-primary hover:text-primary font-black text-white text-base lg:text-xl">Shop Now</button>
+                <button className="py-3 px-10 lg:py-4 xl:py-6 xl:pl-[140px] xl:pr-[132px] bg-primary cursor-pointer hover:bg-transparent border-2 hover:border-primary hover:text-primary font-black text-white text-base lg:text-xl">Shop Now</button>
               </div>
             </div>
           </div>
